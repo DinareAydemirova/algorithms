9df5f7d1a842cba3551751386b2ccdb8991bcbda
DinareAydemirova <dinaraaydamirova@gmail.com>
2024-04-02 19:13:39 +0400

    ALGORITHM

--- js/app.js
+++ js/app.js
@@ -37,8 +37,6 @@
 //     alert("september october november")
 // }
 
-
-
 // let fruit = prompt("name of the fruit");
 // let kg = Number(prompt("enter kg"));
 // let alma = 1;
@@ -55,13 +53,12 @@
 //     break;
 // }
 
-
 // let num= +prompt("enter numbrr")
 // let sum=0
 // for(let i=0; i<num.length; i++){
 //     let number =num[i]
 //     sum+=(number**3)
-   
+
 // }
 // if(sum==num){
 //     console.log(`armstrongdur ${num}`)
@@ -69,11 +66,28 @@
 //     console.log(`armstrong deyil ${num}`)
 // }
 
-
-
 // for(let i=1; i<=1000;i++){
 //     let num=i**2
 //     if(num<1000 && num>10){
 //         console.log(i**2)
 //     }
 // }
+
+// let res = ["12", 5, 5, 6, 7, -7, NaN, undefined, false, null];
+// let res1 = [];
+// for (let i of res) {
+//   if (i * 1 === i) {
+//     res1.push(i);
+//   }
+// }
+// console.log(res1);
+
+let res = [5 , 6, -7,88,9,5,4];
+let num = res[0];
+for (let i=0; i<res.length;i++) {
+  let digit = res[i];
+  if (num < digit) {
+    num = digit;
+  }
+}
+console.log(num);

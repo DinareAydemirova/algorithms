4eda7bfa855a1124f4dab157c418e4aeff397973
glzd222 <mi85h7wkd@code.edu.az>
2024-04-04 19:31:43 +0400

    ALGORITHM

--- js/app.js
+++ js/app.js
@@ -240,19 +240,407 @@
 // }
 // digitCount(numbers)
 
-let sentence = "hello my name is Dinara";
-function LongWord(sentence) {
-  let word = sentence.split(" ");
-  let value = 0;
-  let y;
+// let sentence = "hello my name is Dinara";
+// function LongWord(sentence) {
+//   let word = sentence.split(" ");
+//   let value = 0;
+//   let y;
 
-  for (let i = 0; i < word.length; i++) {
-    let x = word[i];
-    if (x.length > value) {
-      value = x;
-      y = x;
+//   for (let i = 0; i < word.length; i++) {
+//     let x = word[i];
+//     if (x.length > value) {
+//       value = x;
+//       y = x;
+//     }
+//   }
+//   return y;
+// }
+// console.log(LongWord(sentence));
+
+// const fruit= ["banana","orange","apple","mango"]
+// const arr2=[[1,2],[3,4],[5,6]]
+// let result=fruit.length
+// result=fruit.toString()
+// result=fruit.at(3)
+// result=fruit.join(" * ")
+// result=fruit.pop()
+// result=fruit.shift()
+// result=fruit.unshift("sfssdf")
+// result=fruit.delete()
+// result=fruit.concat(arr2)
+// result=fruit.copyWithin(2,3)
+// result=arr2.flat()
+// result=fruit.splice(2, 0, "Lemon", "Kiwi");
+// result=fruit.slice(1)
+
+// onsole.log(result)
+
+let students = [
+  {
+    id: 1,
+    name: "Nurane",
+    surname: "Ismayilzade",
+    age: 21,
+    hobbies: ["music", "walking", "watchingfilm"],
+    student: true,
+    teacher: [
+      { id: 1, name: "gurban" },
+      { id: 2, name: "hajar" },
+      { id: 3, name: "cavid" },
+      { id: 4, name: "alisa" },
+    ],
+    loginDetail: { username: "nurana21", password: "nunu123" },
+    gender: "girl",
+    boyfriendGirlfriend: false,
+    fail: false,
+    avgPoint: 88,
+    salaryAZN: 144,
+  },
+  {
+    id: 2,
+    name: "Gizilgul",
+    surname: "Allahverdiyeva",
+    age: 20,
+    hobbies: ["book", "writing code"],
+    student: true,
+    teacher: [
+      { id: 1, name: "gurban" },
+      { id: 2, name: "hajar" },
+      { id: 3, name: "babaxan" },
+      { id: 4, name: "gulshen" },
+    ],
+    loginDetail: { username: "allahverdieva1", password: "salam123" },
+    gender: "girl",
+    boyfriendGirlfriend: true,
+    fail: false,
+    avgPoint: 70,
+    salaryAZN: 100,
+  },
+  {
+    id: 3,
+    name: "Xanim",
+    surname: "Nuriyeva",
+    age: 21,
+    hobbies: ["book", "music"],
+    student: true,
+    teacher: [
+      { id: 1, name: "gurban" },
+      { id: 2, name: "hajar" },
+      { id: 3, name: "hikmet" },
+      { id: 4, name: "gulsen" },
+    ],
+    loginDetail: { username: "xanim123", password: "salamBaku" },
+    gender: "girl",
+    boyfriendGirlfriend: false,
+    fail: false,
+    avgPoint: 80,
+    salaryAZN: 145,
+  },
+  {
+    id: 4,
+    name: "Minaya",
+    surname: "Binnetova",
+    age: 21,
+    hobbies: ["book", "gaming", "movie", "music"],
+    student: true,
+    teacher: [
+      { id: 1, name: "gurban" },
+      { id: 2, name: "hajar" },
+      { id: 3, name: "anar" },
+    ],
+    loginDetail: { username: "binnetova", password: "hello123" },
+    gender: "girl",
+    boyfriendGirlfriend: false,
+    fail: false,
+    avgPoint: 90,
+    salaryAZN: 142,
+  },
+  {
+    id: 5,
+    name: "Sabina",
+    surname: "Hatamli",
+    age: 21,
+    hobbies: ["shopping", "listen to music"],
+    student: true,
+    teacher: [
+      { id: 1, name: "gurban" },
+      { id: 2, name: "hajar" },
+      { id: 3, name: "Mirvari" },
+    ],
+    loginDetail: { username: "sebine123", password: "salam123" },
+    gender: "girl",
+    boyfriendGirlfriend: true,
+    fail: false,
+    avgPoint: 81,
+    salaryAZN: 146,
+  },
+
+  {
+    id: 6,
+    name: "Ləman",
+    surname: "Şamilova",
+    age: 20,
+    hobbies: [
+      "watching movies",
+      "reading books",
+      "painting",
+      " walking",
+      " listen to music",
+    ],
+    student: true,
+    teacher: [
+      { id: 1, name: "gurban" },
+      { id: 2, name: "hajar" },
+    ],
+    loginDetail: { username: "lemanshamilova", password: "salamBaku" },
+    gender: "girl",
+    boyfriendGirlfriend: false,
+    fail: false,
+    avgPoint: 85.8,
+    salaryAZN: 145,
+  },
+  {
+    id: 7,
+    name: "Narmin",
+    surname: "Musayeva",
+    age: 21,
+    hobbies: ["book", "gaming", "painting", "walking"],
+    student: true,
+    teacher: [
+      { id: 1, name: "gurban" },
+      { id: 2, name: "hajar" },
+      { id: 3, name: "fidan" },
+      { id: 4, name: "Irada" },
+    ],
+    loginDetail: {
+      username: "narmin0",
+      password: "narmin01",
+    },
+    gender: "girl",
+    boyfriendGirlfriend: false,
+    fail: false,
+    avgPoint: 80,
+    salaryAZN: 196,
+  },
+
+  {
+    id: 8,
+    name: "Fatima",
+    surname: "Akhundzada",
+    age: 20,
+    hobbies: ["drawing", "sleeping"],
+    student: true,
+    teacher: [
+      { id: 1, name: "gurban" },
+      { id: 2, name: "hajar" },
+      { id: 3, name: "Valida" },
+    ],
+    loginDetail: { username: "aynur123", password: "salamBaku" },
+    gender: "girl",
+    boyfriendGirlfriend: true,
+    fail: false,
+    avgPoint: 76,
+    salaryAZN: 100,
+  },
+  {
+    id: 9,
+    name: "Elmir",
+    surname: "Huseynov",
+    age: 21,
+    hobbies: ["book", "ice skating", "Tennis", "Karting"],
+    student: true,
+    teacher: [
+      { id: 1, name: "Gurban" },
+      { id: 2, name: "Hajar" },
+      { id: 3, name: "Morad" },
+      { id: 4, name: "Fikrat" },
+    ],
+    loginDetail: { username: "huseynovelmirr", password: "maxverstappen" },
+    gender: "man",
+    boyfriendGirlfriend: false,
+    fail: true,
+    avgPoint: 76,
+    salaryAZN: 120,
+  },
+  {
+    id: 10,
+    name: "Fidan",
+    surname: "Rehimli",
+    age: 21,
+    hobbies: ["book", "gaming"],
+    student: true,
+    teacher: [
+      { id: 1, name: "gurban" },
+      { id: 2, name: "hajar" },
+      { id: 3, name: "Turan" },
+    ],
+    loginDetail: { username: "fidan123", password: "fidanrahimli" },
+    gender: "girl",
+    boyfriendGirlfriend: true,
+    fail: true,
+    avgPoint: 75,
+    salaryAZN: 98,
+  },
+  {
+    id: 11,
+    name: "Aynur",
+    surname: "Aynurova",
+    age: 20,
+    hobbies: ["book", "gaming"],
+    teacher: [
+      { id: 1, name: "gurban" },
+      { id: 2, name: "hajar" },
+    ],
+    loginDetail: { username: "aynur123", password: "salamBaku" },
+    gender: "girl",
+    boyfriendGirlfriend: true,
+    fail: true,
+    avgPoint: 81,
+    salaryAZN: 120,
+  },
+
+  {
+    id: 12,
+    name: "Elman",
+    surname: "Muradov",
+    age: 20,
+    hobbies: [
+      "book",
+      "gaming",
+      "sking",
+      "wrestling",
+      "swiming",
+      "arguing about philosophy",
+      "solving sudoku",
+      "hiking",
+      "walking",
+    ],
+    student: true,
+    teacher: [
+      { id: 1, name: "gurban" },
+      { id: 2, name: "hajar" },
+      { id: 3, name: "jale" },
+      { id: 4, name: "akhmed" },
+    ],
+    loginDetail: { username: "elman17", password: "elman12345" },
+    gender: "girl",
+    boyfriendGirlfriend: false,
+    fail: true,
+    avgPoint: 76,
+    salaryAZN: 100,
+  },
+];
+
+//   function names(students){
+//     for (let i = 0; i < students.length; i++) {
+//        console.log(students[i])
+
+//     }
+//   }
+//   names(students)
+
+// function names(students){
+//     for (let i = 0; i < students.length; i++) {
+//        console.log(students[i].name)
+
+//     }
+//   }
+//   names(students)
+
+// function names(students){
+//     for (let i = 0; i < students.length; i++) {
+//        console.log("ad:",students[i].name , "soyad:",students[i].surname)
+
+//     }
+//   }
+//   names(students)
+
+// function names(students){
+//     for (let i = 0; i < students.length; i++) {
+//       if(students[i].fail){
+//         console.log("ad:",students[i].name , "soyad:",students[i].surname)
+//       }
+
+//     }
+//   }
+//   names(students)
+
+// function hobbies(students) {
+//   let x = "";
+//   let mostHobbies;
+//   for (let i = 0; i < students.length; i++) {
+//     mostHobbies = students[i];
+//     if (mostHobbies.hobbies.length > x.length) {
+//       x = mostHobbies.hobbies;
+//     }
+//   }
+//   console.log("ad:", mostHobbies.name, "hobbies:", mostHobbies.hobbies);
+// }
+// hobbies(students);
+
+// function AvgPoint(students) {
+//   let elment;
+//   let x = 0;
+//   let maxAvg
+//   for (let i = 0; i < students.length; i++) {
+//     elment = students[i];
+//     if (elment.avgPoint > x) {
+//       x = elment.avgPoint;
+//       maxAvg=elment
+
+//     }
+
+//   }
+//   console.log("ad:", maxAvg.name, "soyad:", maxAvg.surname);
+
+// }
+// AvgPoint(students);
+
+// function password(students) {
+//     let elment;
+//     let max = '';
+//     let LongPassword
+//     for (let i = 0; i < students.length; i++) {
+//       elment = students[i];
+//       if (elment.loginDetail.password.length > max.length) {
+//         max = elment.loginDetail.password;
+//         LongPassword=elment
+
+//       }
+
+//     }
+//     console.log("ad:", LongPassword.name, "soyad:", LongPassword.surname);
+
+//   }
+//   password(students);
+
+// function password(students) {
+//   let element;
+//   for (let i = 0; i < students.length; i++) {
+//     element = students[i];
+//     if (element.boyfriendGirlfriend) {
+//       console.log("ad:" + element.name + "soyad:" + element.loginDetail.username);
+//     }
+//   }
+// }
+// password(students);
+
+
+
+function password(arr) {
+    let element;
+    for (let i = 0; i < students.length; i++) {
+      element = students[i];
+      let teacherBox
+      if (element.age==20) {
+        for (let j = 0; j < element.teacher.length; j++) {
+           
+           teacherBox+= arr[i].teacher[j].name+" "
+        }
+       
+      }
+      console.log(`ad: ${arr[i].name} teacher: ${teacherBox.name}`);
+
     }
   }
-  return y;
-}
-console.log(LongWord(sentence));
+  password(arr);
